aecfc1ff6be88d7d055a5fd74c50cfbee926411c
Maddy <mmcintosh2k14@gmail.com>
2020-01-04 18:30:11 -0500

    creates server.js

---
+++ api/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+
+// const authenticate = require('../middleware/authenticate-middleware'); authentication middleware
+// const authRouter = require('../auth/auth-router);
+
+const server = express();
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+server.use('/api/auth', authRouter);
+
+module.exports = server;
