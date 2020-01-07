39a2e8a4f2c391dd21f818bbac0b4c82814b6603
Maddy <mmcintosh2k14@gmail.com>
2020-01-07 12:08:08 -0500

    creates DELETE endpoint in petsRouter

--- pets/pets-router.js
+++ pets/pets-router.js
@@ -10,7 +10,12 @@ router.put('/:id', (req, res) => {
 	Pets.update(id, changes)
 		.then(pet => {
 			if (pet) {
-				res.status(200).json(pet);
+				res
+					.status(200)
+					.json({
+						message: `Pet with ID ${id} was successfully updated!`,
+						pet
+					});
 			} else {
 				res.status(404).json({ message: 'The pet could not be found.' });
 			}
@@ -20,3 +25,24 @@ router.put('/:id', (req, res) => {
 			res.status(500).json({ error: 'Error editing gigapet.' });
 		});
 });
+
+// DELETE endpoint to delete gigapet
+router.delete('/:id', (req, res) => {
+	const { id } = req.params;
+
+	Pets.remove(id)
+		.then(count => {
+			if (count > 0) {
+				res
+					.status(200)
+					.json({ message: 'The pet has been successfully deleted!' });
+			} else {
+				res.status(404).json({ message: 'The pet could not be deleted.' });
+			}
+		})
+		.catch(err => {
+			console.log('Error deleting pet.', err);
+			res.status(500).json({ error: 'Error deleting pet.' });
+		});
+});
+module.exports = router;
