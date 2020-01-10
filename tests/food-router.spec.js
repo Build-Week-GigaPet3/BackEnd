350e50d2d70de5805758d3b5e211d2a21d25e9ec
Maddy <mmcintosh2k14@gmail.com>
2020-01-10 16:30:44 -0500

    creates GET food log endpoint test

--- tests/food-router.spec.js
+++ tests/food-router.spec.js
@@ -13,7 +13,7 @@ describe('Food Router functionality', () => {
 		expect(response.type).toMatch(/json/i);
 	});
 
-	it('POST should create a new food log for logged in parent.', () => {
+	it('POST should return error for missing info.', () => {
 		const parent1 = { username: 'Des', password: 'navysailor' };
 
 		return request(server)
@@ -22,14 +22,14 @@ describe('Food Router functionality', () => {
 			.then(() => {
 				return request(server)
 					.post('/api/auth/login')
-          .send(parent1)
-          .then(res => {
+					.send(parent1)
+					.then(res => {
 						const token = res.body.token;
 
 						return request(server)
-              .post('/api/parents/1/food/logs')
-              .set("authorization", token)
-							.send({ food_item: 'ham', food_catagory_id: 4, parent_id: 1 })
+							.post('/api/parents/1/food/logs')
+							.set('authorization', token)
+							.send({ food_item: 'ham', parent_id: 1 })
 							.then(res => {
 								expect(res.status).toBe(400);
 								expect(res.body).toEqual({
@@ -43,47 +43,62 @@ describe('Food Router functionality', () => {
 							});
 					});
 			});
+	});
+
+	it('GET request should return 200 status code and JSON object of food logs for specified account.', () => {
+		const parent2 = { username: 'Benny', password: 'broham' };
+
+		return request(server)
+			.post('/api/auth/register')
+			.send(parent2)
+			.then(() => {
+				return request(server)
+					.post('/api/auth/login')
+					.send(parent2)
+					.then(res => {
+						const token = res.body.token;
+
+						return request(server)
+							.post('/api/parents/1/food/logs')
+							.set('authorization', token)
+							.send({ food_item: 'ham', food_category_id: '4', parent_id: 1 })
+							.expect(201)
+							.then(() => {
+								return request(server)
+									.get('/api/parents/1/food/logs')
+									.set('authorization', token)
+									.expect(200);
+							});
+					});
+			});
   });
   
-  it('GET request should return 200 status code and JSON object of food logs for specified account.', () => {
-    
-  })
-
-	// const id = 2;
-
-	// it('should return an 400 error with info for required field.', function() {});
-
-	// it('GET request should return 200 status code and JSON object of food logs for specified parent ID', function() {
-	// 	return request(server)
-	// 		.get(`/api/parents/${id}/food/logs`)
-	// 		.then(res => {
-	// 			expect(res.status).toBe(200);
-	// 			expect(res.type).toMatch(/json/i);
-	// 		});
-	// });
-
-	// const logId = 1;
-
-	// it('PUT request should return JSON object upon successful update', function() {
-	// 	return request(server)
-	// 		.put(`/api/food/logs/${logId}`)
-	// 		.send({ food_item: 'banana' })
-	// 		.then(res => {
-	// 			expect(res.type).toMatch(/json/i);
-	// 			expect(res.body).toEqual({
-	// 				message: 'Food log update was successful.',
-	// 				log: 1
-	// 			});
-	// 		});
-	// });
-
-	// it('DELETE request should return success message upon deletion', function() {
-	// 	return request(server)
-	// 		.delete(`/api/food/logs/${logId}`)
-	// 		.then(res => {
-	// 			expect(res.body).toEqual({
-	// 				message: 'The food log was deleted successfully!'
-	// 			});
-	// 		});
-	// });
+  
 });
+
+
+// const logId = 1;
+
+// it('PUT request should return JSON object upon successful update', function() {
+// 	return request(server)
+// 		.put(`/api/food/logs/${logId}`)
+// 		.send({ food_item: 'banana' })
+// 		.then(res => {
+// 			expect(res.type).toMatch(/json/i);
+// 			expect(res.body).toEqual({
+// 				message: 'Food log update was successful.',
+// 				log: 1
+// 			});
+// 		});
+// });
+
+// it('DELETE request should return success message upon deletion', function() {
+// 	return request(server)
+// 		.delete(`/api/food/logs/${logId}`)
+// 		.then(res => {
+// 			expect(res.body).toEqual({
+// 				message: 'The food log was deleted successfully!'
+// 			});
+// 		});
+// });
+// });
